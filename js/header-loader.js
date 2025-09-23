8e478fac0d53bffba2779767b187f71ee3861999
Juan Felipe Arias Aranguren <juanfelipeariasaranguren@gmail.com>
2025-09-22 22:40:18 -0500

    Fix mobile dropdowns: load Bootstrap JS after header injection
    Now menus open correctly on mobile when tapped.

--- js/header-loader.js
+++ js/header-loader.js
@@ -1,9 +1,14 @@
 const depth = window.location.pathname.split('/').length - 2;
 const basePath = '../'.repeat(depth);
 const headerPath = basePath + 'header.html';
+const bootstrapPath = basePath + 'js/bootstrap.bundle.min.js';
 
 fetch(headerPath)
   .then(res => res.text())
   .then(html => {
     document.getElementById('header-placeholder').innerHTML = html;
+
+    const bootstrapScript = document.createElement('script');
+    bootstrapScript.src = basePath + 'js/bootstrap.bundle.min.js';
+    document.body.appendChild(bootstrapScript);
   });
